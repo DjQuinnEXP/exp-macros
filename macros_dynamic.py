ba0b370d4dedbc3c877cba567711bf6c1daeaa75
Quinn Stadens <quinnstadens@happyhorizon.com>
2026-05-01 17:17:43 +0200

    [BUGFIX] Made sure the script properly works on wayland as well now, cleanup

--- macros_dynamic.py
+++ macros_dynamic.py
@@ -19,21 +19,19 @@ for device in devices:
     if deviceToLookFor in device.name and 'input0' in device.phys:
         dev = evdev.InputDevice(device.path)
         break
-
-# DISABLE ALL DEVICES THAT CORRESPOND WITH THE ONE I PUT AS MY MACRO DEVICE
-xinput_devices = os.popen("xinput list | grep 'SEMICO' | grep 'keyboard' | cut -d '=' -f 2 | cut -f 1").read()
-xinput_devices_array = xinput_devices.split('\n')
-for xinput_device in xinput_devices_array:
-    os.system('/usr/bin/xinput --disable ' + xinput_device)
-    print('disabled device with id:', xinput_device)
-
 if dev is None:
     sys.exit("No device was found")
 
+# To make sure input is isolated to this script
+dev.grab()
 for event in dev.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
         key = evdev.categorize(event)
         if key.keystate == key.key_down:
+            if key.keycode == 'KEY_F1':
+                os.system('apt-get update')
+            if key.keycode == 'KEY_F2':
+                os.system('apt-get upgrade')
             if key.keycode == 'KEY_Q':
                 os.system('/bin/bash '+ scriptpath + 'warden_start_list.sh')
             if key.keycode == 'KEY_W':
